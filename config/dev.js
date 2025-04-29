eb29c611cec6e7f73ad2540f6ce0077d1f91f7d6
jigaradesara-bacancy <jigar.adesara@bacancy.com>
2025-04-29 17:45:28 +0530

    initial commit

--- config/dev.js
+++ config/dev.js
@@ -1,7 +1,8 @@
 module.exports = {
   googleClientID:
-    '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
-  googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
-  cookieKey: '123123123',
+    "301239130801-a2ce1sbtbetqkp5tc66891khu5d87dn8.apps.googleusercontent.com",
+  googleClientSecret: "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",
+  mongoURI:
+    "mongodb+srv://jigaradesara:S6N4bTxqqwvmZds5@cluster0.luehg2a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+  cookieKey: "123123123",
 };
