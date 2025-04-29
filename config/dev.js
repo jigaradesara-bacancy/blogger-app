7da233e56f00a8df08c382ab6acecdf46e182623
jigaradesara-bacancy <jigar.adesara@bacancy.com>
2025-04-29 17:53:44 +0530

    Update dev.js

--- config/dev.js
+++ config/dev.js
@@ -1,8 +1,8 @@
 module.exports = {
   googleClientID:
-    "301239130801-a2ce1sbtbetqkp5tc66891khu5d87dn8.apps.googleusercontent.com",
-  googleClientSecret: "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",
+    "",
+  googleClientSecret: "",
   mongoURI:
-    "mongodb+srv://jigaradesara:S6N4bTxqqwvmZds5@cluster0.luehg2a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-  cookieKey: "123123123",
+    "",
+  cookieKey: "",
 };
